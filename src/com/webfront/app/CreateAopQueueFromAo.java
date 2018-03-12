8a5a6f21e03e4096d48566901e05c6e7d26827fa
rlittle <rlittle@littleacres.org>
2018-03-12 14:37:56 -0400

    CreateAopQueueFromAo: Populate iList items from responses entered into PromptDialog

--- src/com/webfront/app/CreateAopQueueFromAo.java
+++ src/com/webfront/app/CreateAopQueueFromAo.java
@@ -12,6 +12,7 @@ import static asjava.uniobjects.UniObjectsTokens.LOCK_NO_LOCK;
 import asjava.uniobjects.UniSelectList;
 import asjava.uniobjects.UniSelectListException;
 import asjava.uniobjects.UniSessionException;
+import com.webfront.u2.model.Prompt;
 import com.webfront.u2.model.UvData;
 import com.webfront.util.FileUtils;
 import java.util.ArrayList;
@@ -26,12 +27,37 @@ public class CreateAopQueueFromAo extends BaseApp {
 
     boolean isHistory;
     UvData aopQueueRec;
-    final int RUN_LEVEL = 3;
+    int runLevel = -1;
+    String queueType;
+    String queueGroup;
     UvData aoRec;
 
     @Override
     public boolean mainLoop() {
         try {
+            for (Prompt prp : program.getPromptList()) {
+                if (prp.getMessage().equalsIgnoreCase("Run level")) {
+                    runLevel = Integer.parseInt(prp.getResponse());
+                }
+                if (prp.getMessage().equalsIgnoreCase("Queue type")) {
+                    queueType = prp.getResponse();
+                }
+                if (prp.getMessage().equalsIgnoreCase("Queue group")) {
+                    queueGroup = prp.getResponse();
+                }
+            }
+            if (runLevel == -1) {
+                progress.display("Run level is required");
+                return false;
+            }
+            if (queueType.isEmpty()) {
+                progress.display("Queue type is required");
+                return false;
+            }
+            if (queueGroup.isEmpty()) {
+                progress.display("Queue group is required");
+                return false;
+            }
             UniSelectList list3 = readSession.selectList(3);
             list3.getList(listName);
             UniDynArray temp = list3.readList();
@@ -61,36 +87,36 @@ public class CreateAopQueueFromAo extends BaseApp {
                     continue;
                 }
                 orkList.add(orkId);
-                
+
                 String vendorId = aoRec.getData().extract(181, 1).toString();
                 String[] tmp = orkId.split("\\*");
-                String key = vendorId+"*"+tmp[1] + '*' + tmp[2];
+                String key = vendorId + "*" + tmp[1] + '*' + tmp[2];
                 if (keyList.contains(key)) {
                     continue;
                 }
                 keyList.add(key);
-                
-                if(!getNextQueue()) {
+
+                if (!getNextQueue()) {
                     progress.display("Can't get AOP.QUEUE record");
                     continue;
                 }
-                
+
                 aopQueueRec.getData().replace(1, vendorId);
-                aopQueueRec.getData().replace(4, "3");
+                aopQueueRec.getData().replace(4, Integer.toString(runLevel));
                 aopQueueRec.getData().replace(5, tmp[1]);
                 aopQueueRec.getData().replace(6, tmp[2]);
                 aopQueueRec.getData().replace(7, "AOPSUPPORT");
-                aopQueueRec.getData().replace(14, "AO");
-                aopQueueRec.getData().replace(20, "A");
-                for(int i = 0 ; i< RUN_LEVEL; i++) {
-                    aopQueueRec.getData().insert(24, 1 ,i);
-                    aopQueueRec.getData().insert(25, 1 ,tmp[1]);
-                    aopQueueRec.getData().insert(26, 1 ,tmp[2]);
-                    aopQueueRec.getData().insert(27, 1 ,tmp[1]);
-                    aopQueueRec.getData().insert(28, 1 ,tmp[2]);                    
-                    aopQueueRec.getData().insert(29, 1 ,"0");
+                aopQueueRec.getData().replace(14, queueType);
+                aopQueueRec.getData().replace(20, queueGroup);
+                for (int i = 0; i < runLevel; i++) {
+                    aopQueueRec.getData().insert(24, 1, i);
+                    aopQueueRec.getData().insert(25, 1, tmp[1]);
+                    aopQueueRec.getData().insert(26, 1, tmp[2]);
+                    aopQueueRec.getData().insert(27, 1, tmp[1]);
+                    aopQueueRec.getData().insert(28, 1, tmp[2]);
+                    aopQueueRec.getData().insert(29, 1, "0");
                 }
-                
+
                 int result = FileUtils.writeRecord(writeFiles.get("AOP.QUEUE"), aopQueueRec);
             }
             teardown();
@@ -142,10 +168,10 @@ public class CreateAopQueueFromAo extends BaseApp {
         result = FileUtils.getRecord(readFiles.get("SEQ.FILE"), seqRec);
         String queueId = seqRec.getData().extract(1).toString();
         nextQueue = Integer.parseInt(queueId);
-        
+
         seqRec.getData().replace(1, Integer.toString(nextQueue + 1));
         result = FileUtils.writeRecord(writeFiles.get("SEQ.FILE"), seqRec);
-        
+
         aopQueueRec = new UvData();
         aopQueueRec.setId(queueId);
         result = FileUtils.lockRecord(readFiles.get("AOP.QUEUE"), aopQueueRec);
